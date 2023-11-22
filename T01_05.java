11d9e1b83ee790159da32f78f95d9ea880b06671
avimanyurimal <rimal.avimanyu@gmail.com>
2023-11-22 17:40:55 +0545

    first commit

---
+++ T01_05.java
@@ -0,0 +1,17 @@
+package tutorial_workshop_01;
+
+public class T01_05 {
+
+	public static void main(String[] args) {
+		int a = 10, b = 5;
+		int addition = a + b;
+		int subtraction = a - b;
+		int multiplication = a * b;
+		int division = a / b;
+		System.out.println(addition);
+		System.out.println(subtraction);
+		System.out.println(multiplication);
+		System.out.println(division);
+	}
+
+}
---
+++ T01_09.java
@@ -0,0 +1,14 @@
+package tutorial_workshop_01;
+
+public class T01_09 {
+
+	public static void main(String[] args) {
+		int length = 10;
+		int width = 5;
+		int area = length * width;
+		System.out.println(area);
+
+
+	}
+
+}
