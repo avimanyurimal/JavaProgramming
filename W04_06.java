9767e1275952f9ae6d0abf09c944c059e6083e27
Avimanyu Rimal <127291200+avimanyurimal@users.noreply.github.com>
2024-01-10 23:35:38 +0545

    Workshop04 Second Commit

---
+++ W04_06.java
@@ -0,0 +1,17 @@
+package tutorial_workshop04;
+
+public class W04_06 {
+
+	public static void main(String[] args) {
+		Users user = new Users();
+	}
+
+}
+
+class Users{
+	
+	public Users() {
+		System.out.println("User Created!");
+	}
+	
+}
