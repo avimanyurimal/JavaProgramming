8801024d1caddef8f9a1b6f7a43316f3e608d82c
Avimanyu Rimal <127291200+avimanyurimal@users.noreply.github.com>
2024-01-13 22:46:28 +0545

    Tutorial_05_Second Commit

---
+++ T05_02.java
@@ -0,0 +1,16 @@
+package tutorialworkshop05;
+
+public class T05_02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+abstract class Bankss {
+	void getBalance() {
+		
+	}
+}
