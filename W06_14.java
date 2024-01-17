63f2b5a2f7033c29580ff077acbc05d347e45968
Avimanyu Rimal <127291200+avimanyurimal@users.noreply.github.com>
2024-01-17 22:35:02 +0545

    Workshop_06_SecondCommit

---
+++ W06_14.java
@@ -0,0 +1,13 @@
+package tutorialworkshop_06;
+
+public class W06_14 {
+
+	public static void main(String[] args) {
+		try {
+			System.out.println(10/0);
+		} catch (Exception e){
+			System.out.println("Error: "+e);
+		}
+	}
+}
+
