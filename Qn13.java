dcfcfa9e5bf67fc8ab0c56d6b585485d91916be2
Avimanyu Rimal <127291200+avimanyurimal@users.noreply.github.com>
2024-01-29 20:50:53 +0545

    Workshop

---
+++ Qn13.java
@@ -0,0 +1,18 @@
+package tutorialworkshop09;
+import java.util.*;
+public class Qn13 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TreeSet <String> names = new TreeSet<>();
+		
+		//adding the names
+		names.add("Surendra");
+		names.add("Samyog");
+		names.add("Avimanyu");
+		
+		//displaying the set
+		System.out.println(names);
+	}
+
+}
